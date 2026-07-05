7a320cdfe0bfbe5a07cff6ab2e38a341d9ad5a80
Mako <422926289@qq.com>
2026-07-05 22:41:25 +0800

    Add package init

---
+++ src/china_travel_mcp/__init__.py
@@ -0,0 +1,8 @@
+"""
+China Travel MCP - Inbound travel to China.
+5 tools: search_hotels, search_flights, search_attractions, plan_itinerary, get_travel_tips
+"""
+
+from .server import mcp, main
+
+__all__ = ["mcp", "main"]
